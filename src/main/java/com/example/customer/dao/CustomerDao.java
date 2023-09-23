96e2ae1d2cc5ef3471075d25affda7cb6096f1c0
Shan <perera.alc2000@gmail.com>
2023-09-23 09:55:42 +0530

    super-entity

--- src/main/java/com/example/customer/dao/CustomerDao.java
+++ src/main/java/com/example/customer/dao/CustomerDao.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 public interface CustomerDao extends JpaRepository<Customer, String> {
     Customer save(Customer customer);
+
     Customer getCustomerById(String id);
+
     @Transactional
     @Query("UPDATE Customer c SET c.name = :name, c.city = :city, c.email = :email WHERE    c.id = :id")
     int updateCustomer(@Param("name") String name, @Param("city") String city, @Param("email") String email, @Param("id") String id);
---
+++ src/main/java/com/example/customer/entity/SuperEntity.java
@@ -0,0 +1,4 @@
+package com.example.customer.entity;
+
+public interface SuperEntity {
+}
