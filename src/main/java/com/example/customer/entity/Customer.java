b9b8453dad81d780da636d1930a4828e52324e07
Shan <perera.alc2000@gmail.com>
2023-10-03 23:19:00 +0530

    NonNull

--- src/main/java/com/example/customer/entity/Customer.java
+++ src/main/java/com/example/customer/entity/Customer.java
@@ -1,10 +1,12 @@
 package com.example.customer.entity;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
 
 @Entity
 @Data
@@ -13,6 +15,7 @@ import lombok.NoArgsConstructor;
 public class Customer implements SuperEntity {
     @Id
     private String id;
+    @NonNull
     private String name;
     private String city;
     private String email;
